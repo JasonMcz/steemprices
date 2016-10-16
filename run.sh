a5bf04a62d62673ad1ca61d5b21ef2e4cca97e2d
Joe Zhou <bitcoin2095@gmail.com>
2016-10-16 14:47:31 +0800

    CHANGED: Run.sh script auto run after server/api connection crash.

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 http-server -p 80 -c-1 ./app &
-while :
-do
-  babel-node get_data.js
-  babel-node get_market.js
-  sleep 15
+
+running=1
+
+finish()
+{
+    running=0
+}
+
+trap finish SIGINT
+
+while (( running )); do
+    // Execute the command here that starts your server.
+    echo "Restarting server on crash.."
+    babel-node get_data.js
+    babel-node get_market.js
+    sleep 30
 done
